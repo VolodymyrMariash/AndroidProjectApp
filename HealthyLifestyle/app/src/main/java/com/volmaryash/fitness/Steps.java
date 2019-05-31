3c9c68fd99d707761108addc37d6d35bbc5f04ca
Volodymyr Mariash <volodymyr.mariash.kn.2016@lpnu.ua>
2019-05-31 18:57:53 +0300

    mobile app KN-318

---
+++ HealthyLifestyle/app/src/main/java/com/volmaryash/fitness/Steps.java
@@ -0,0 +1,13 @@
+package com.volmaryash.fitness;
+
+public class Steps {
+    public int totalsteps;
+
+    public Steps() {
+        // Default constructor required for calls to DataSnapshot.getValue(Steps.class)
+    }
+
+    public Steps(int totalsteps) {
+        this.totalsteps = totalsteps;
+    }
+}
